3722ad68e3d29e5cdd29e99acd461d16dc098692
liushooter <shooter.hero@gmail.com>
2022-07-29 17:41:49 +0800

    private Attributes

--- config/api.js
+++ config/api.js
@@ -1,7 +1,11 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  // https://strapi.io/blog/postgre-sql-and-strapi-setup
+  responses: {
+    privateAttributes: ['_v', 'id', 'createdAt', 'updatedAt'],
+  },
   rest: {
     defaultLimit: 25,
     maxLimit: 100,
     withCount: true,
   },
-};
+});
